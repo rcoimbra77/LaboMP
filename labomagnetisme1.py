be84c71d82e56955dc92b319e1690ee847e8c743
Joseph7457 <78625758+Joseph7457@users.noreply.github.com>
2022-02-09 10:12:09 +0100

    Update labomagnetisme1.py

--- labomagnetisme1.py
+++ labomagnetisme1.py
@@ -8,7 +8,12 @@ import sys
 BLEUCLAIR = (127, 191, 255)
 NOIR      = (0,0,0)
 ROUGE     = (255,0,0)
+A = 2
+B = 5
+C = 20
 #k = 8 987 600 000
+k = 8.9876 * pow(10,9)
+
 
 # La norme de ce vecteur est égale à k|q|r2, où r est la distance qui sépare p et p′, 
 # Paramètres
@@ -16,6 +21,40 @@ ROUGE     = (255,0,0)
 dimensions_fenetre = (1600, 900)  # en pixels
 images_par_seconde = 25
 objets = []
+
+def deplacer_pol(point, distance, orientation):
+    x, y = point
+
+    x = point[0] + (distance * math.cos(orientation))
+    y = point[1] + (distance * math.sin(orientation))
+
+    return (x, y)
+
+def dessiner_vecteur(fenetre, couleur, origine, vecteur):
+
+    p = origine
+    p4 = p + vecteur
+    angle = math.atan2(vecteur[1], vecteur[0])
+
+    if math.sqrt(vecteur[1]**2 + vecteur[0]**2) >= 20:
+        p4 = (p[0] + vecteur[0], p[1] + vecteur[1])
+        pp4 = math.sqrt(vecteur[1]**2 + vecteur[0]**2)
+        p1 = deplacer_pol(p, A, angle - math.pi/2)
+        p7 = deplacer_pol(p, A, angle + math.pi/2)
+        p2 = deplacer_pol(p1, pp4 - C, angle)
+        p6 = deplacer_pol(p7, pp4 - C, angle)
+        p3 = deplacer_pol(p2, B, angle - math.pi/2)
+        p5 = deplacer_pol(p6, B, angle + math.pi/2)
+
+        pygame.draw.polygon(fenetre, couleur, [p1, p2, p3, p4, p5, p6, p7])
+
+    else:
+        p3 = (p[0] + vecteur[0], p[1] + vecteur[0])
+        p1 = deplacer_pol(p3, C, angle + math.pi)
+        p2 = deplacer_pol(p1, A + B, angle - math.pi/2)
+        p4 = deplacer_pol(p1, A + B, angle + math.pi/2)
+
+        pygame.draw.polygon(fenetre, couleur, [p1, p2, p3, p4])
 
 def ajouter_objet(x,y,z):
     objets.append((x,y,z))
@@ -41,22 +80,48 @@ def dessiner_champ(pas):
     while(x < dimensions_fenetre[0] + pas):
         y = -pas
         while(y < dimensions_fenetre[1] + pas):
-            print(x)
-            print(y)
+            vecteur = calculer_champ(x,y)
+            vecteur = normer_vecteur(40, vecteur)
+            dessiner_vecteur(fenetre, ROUGE, (x,y), (vecteur[0], vecteur[1]))
+            print(vecteur[0])
+            print(vecteur[1])
             print(" ")
             y += pas
         x += pas
 
-dessiner_champ(50)
+def normer_vecteur(tailleMax, v):
+    norme   = math.sqrt(v[0]*v[0] + v[1]*v[1])
+    vecteur = [0,0]
+    if (norme !=0):
+        vecteur = [ v[0]*tailleMax/norme, v[1]*tailleMax/norme ]
+    return vecteur
 
 
-print_objets()
+def calculer_champ(x,y):
+    norme = 0
+    v = [0,0]
+    for o in objets:
+        r     = math.sqrt( (o[0]-x) * (o[0]-x) + (o[1]-y) * (o[1]-y) )
+        if(r > 20):
+            norme = k * abs(o[2]) / (r*r)
+        angle = math.atan2( (o[0]-x), (o[1]-y) )
+        vtemp = (norme * math.cos(angle) , norme * math.sin(angle))
+        v[0] += vtemp[0]
+        v[1] += vtemp[1]    
+    return v    
+
+
+
+print(k)
+
+#print_objets()
 # Initialisation
 
 pygame.init()
 
 fenetre = pygame.display.set_mode(dimensions_fenetre)
 pygame.display.set_caption("Programme 1")
+
 
 horloge = pygame.time.Clock()
 couleur_fond = BLEUCLAIR
@@ -72,6 +137,6 @@ while True:
             sys.exit()
     
     dessiner_objets()
-
+    dessiner_champ(50)
     pygame.display.flip()
     horloge.tick(images_par_seconde)
