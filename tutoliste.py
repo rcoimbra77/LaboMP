01d172f724e98c070db13414aac9e19c93ccd8b8
Joseph7457 <78625758+Joseph7457@users.noreply.github.com>
2022-02-10 17:55:56 +0100

    Update tutoliste.py

--- tutoliste.py
+++ tutoliste.py
@@ -7,6 +7,7 @@ print("# une liste peut se déclarer ainsi:")
 print("liste = []")
 
 liste = []
+print("contenu de la liste")
 print(liste)
 
 
@@ -15,6 +16,8 @@ print("# .append(élément) permet d'ajouter un élément à la liste")
 print("liste.append(45)")
 
 liste.append(45)
+
+print("contenu de la liste")
 print(liste)
 
 # on peut mélanger les types d'élements avec python
@@ -24,6 +27,8 @@ print("# on peut mélanger les types d'élements avec python")
 liste.append('a')
 liste.append(42)
 liste.append("blablabla")
+
+print("contenu de la liste")
 print(liste)
 
 # même des tuples 
@@ -34,6 +39,8 @@ print("liste.append( (4,5,4,'a')  )")
 
 liste.append( (2,4) )
 liste.append( (4,5,4,'a')  )
+
+print("contenu de la liste")
 print(liste)
 
 # même des listes
@@ -42,12 +49,22 @@ print("# même des listes")
 
 liste.append([])
 liste.append([[]])
+
+print("contenu de la liste")
 print(liste)
 
 #on accede à un element de la liste avec son index
 print(" ")
 print("# on accede à un element de la liste avec son index")
 
+
+print("contenu de la liste en 0")
 print(liste[0])
+
+print("contenu de la liste en 1")
 print(liste[1])
+
+
+
+print("etc...")
 print(liste[etc])
